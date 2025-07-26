22af81c1225bfda17ee0d7fbf37d6ebf9236db75
Shivam Gupta <shivamgupta191092005@gmail.com>
2025-07-26 23:05:58 +0530

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "BeyondCareer" },
+    { name: "description", content: "Welcome to BeyondCareer!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat">
+    <Navbar />
+   <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Rating</h1>
+        <h2>Review Your Submission and check AI-powered feedback</h2>
+      </div>
+   </section>
+  </main>;
 }
