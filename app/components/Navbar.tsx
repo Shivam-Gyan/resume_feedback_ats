543b0ec5a5f456597e8c77e35882c85a299d26fd
Shivam Gupta <shivamgupta191092005@gmail.com>
2025-07-27 00:15:51 +0530

    Add resume management features and integrate Puter.js for enhanced functionality
    - Updated .gitignore to include .env
    - Enhanced Navbar with links to home and upload sections
    - Created ResumeCard component for displaying resumes
    - Implemented ScoreCircle component for visual score representation
    - Added Puter.js integration for authentication and file management
    - Developed usePuterStore for state management with Zustand
    - Updated Home component to display resumes dynamically
    - Introduced constants for sample resumes
    - Defined types for Job, Resume, Feedback, and Puter-related interfaces

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <nav className='navbar'>
+            <Link to="/">
+                <p className='text-2xl font-bold text-gradient'>BeyondCareer</p>
+            </Link>
+            <Link to={'/upload'}>
+                <p className='primary-button w-fit'>Upload Resume</p>
+            </Link>
+        </nav>
+    )
 }
 
 export default Navbar
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,9 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants/index";
+import ResumeCard from "~/components/Resume.card";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "BeyondCareer" },
     { name: "description", content: "Welcome to BeyondCareer!" },
@@ -9,13 +11,31 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat">
+  return (<main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat">
     <Navbar />
-   <section className="main-section">
-      <div className="page-heading">
+    <section className="main-section">
+      <div className="page-heading py-16">
         <h1>Track Your Application & Resume Rating</h1>
         <h2>Review Your Submission and check AI-powered feedback</h2>
       </div>
-   </section>
-  </main>;
+
+
+      {
+        resumes.length > 0 && (
+          <div className="resumes-section">
+            {
+              resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+              ))
+            }
+          </div>
+        )
+      }
+    </section>
+
+
+  </main>);
 }
+
+
+
